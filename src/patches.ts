7e0e37a77455291eb2034b4091c575501f5e9830
NotNite <hi@notnite.com>
2024-11-21 09:28:46 -0500

    Fix colors

--- src/patches.ts
+++ src/patches.ts
@@ -14,24 +14,23 @@ export const patches: WebpackToolsPatch[] = [
       {
         match: /{start:"#[0-9a-fA-F]{6}",end:(.{1,3}\.blue3)}/g,
         replacement: (_, end) => `{start:"${newColor}",end:${end}}`
+      },
+      {
+        match: /brandBlue:"#[0-9a-fA-F]{6}"/g,
+        replacement: `brandBlue:"${newColor}"`
+      },
+      {
+        match: /primary_(\d{2,3}):`hsl\(\${t.primary}, 99%, (\d{1,2})%\)`/g,
+        replacement: (_, num, percent) => `primary_${num}:\`${newColor}\``
       }
     ]
   },
-  // TODO this doesnt work
   {
     name: "brandColor2",
     find: /#1083fe/g,
     replace: {
       match: /#1083fe/g,
       replacement: newColor
-    }
-  },
-  {
-    name: "birthday",
-    find: "Birthday",
-    replace: {
-      match: /Birthday/g,
-      replacement: "meow"
     }
   }
 ];
